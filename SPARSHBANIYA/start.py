a72019447a21bdd0ea16b320c6b07cdf4fc52621
ꜱᴘᴧʀꜱʜ ʙᴧɴɪʏᴧ <89186991+TeamBaniya@users.noreply.github.com>
2026-05-08 19:37:48 +0530

    Update start.py

--- SPARSHBANIYA/start.py
+++ SPARSHBANIYA/start.py
@@ -35,8 +35,8 @@ async def start(bot: Client, msg: Message):
                     InlineKeyboardButton(text="✨ ɢᴇɴᴇʀᴀᴛᴇ ɴᴏᴡ ✨", callback_data="generate", style=ButtonStyle.PRIMARY)
                 ],
                 [
-                    InlineKeyboardButton("💬 sᴜᴘᴘᴏʀᴛ", url="https://t.me/BANIYA_BOTS", style=ButtonStyle.SUCCESS),
-                    InlineKeyboardButton("📡 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/BANIYA_UPDATES", style=ButtonStyle.SUCCESS)
+                    InlineKeyboardButton("💬 sᴜᴘᴘᴏʀᴛ", url="https://t.me/AURA_NETWORKS", style=ButtonStyle.SUCCESS),
+                    InlineKeyboardButton("📡 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/AURA_NETWORKS", style=ButtonStyle.SUCCESS)
                 ],
                 [
                     InlineKeyboardButton("🔗 sᴏᴜʀᴄᴇ", url="https://github.com/TeamBaniya/BANIYA_STRING", style=ButtonStyle.PRIMARY),
@@ -131,11 +131,11 @@ async def back_to_start(bot: Client, callback_query):
                     InlineKeyboardButton(text="✨ ɢᴇɴᴇʀᴀᴛᴇ ɴᴏᴡ ✨", callback_data="generate", style=ButtonStyle.PRIMARY)
                 ],
                 [
-                    InlineKeyboardButton("💬 sᴜᴘᴘᴏʀᴛ", url="https://t.me/GIRLSSOCIETYLINK", style=ButtonStyle.SUCCESS),
-                    InlineKeyboardButton("📡 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/Requirements_json", style=ButtonStyle.SUCCESS)
+                    InlineKeyboardButton("💬 sᴜᴘᴘᴏʀᴛ", url="https://t.me/AURA_NETWORKS", style=ButtonStyle.SUCCESS),
+                    InlineKeyboardButton("📡 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/AURA_NETWORKS", style=ButtonStyle.SUCCESS)
                 ],
                 [
-                    InlineKeyboardButton("🔗 sᴏᴜʀᴄᴇ", url="https://github.com/TeamBaniya/BANIYA_STRING", style=ButtonStyle.PRIMARY),
+                    InlineKeyboardButton("🔗 sᴏᴜʀᴄᴇ", url="https://t.me/AURA_NETWORKS", style=ButtonStyle.PRIMARY),
                     InlineKeyboardButton("❓ ʜᴇʟᴘ", callback_data="help", style=ButtonStyle.PRIMARY)
                 ]                
             ]
